8573a7ab25ded3aea60aa2c5771ef187ac703ceb
kadhir <kadhir@kadhir.com>
2024-03-08 15:53:40 +0530

    Implement CourseListView and CourseDetailView for displaying available courses to public

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.list import ListView
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -117,4 +117,42 @@ class ModuleContentListView(TemplateResponseMixin, View):
 
     def get(self, request, module_id):
         module = get_object_or_404(Module, id=module_id, course__owner=request.user)
+<<<<<<< HEAD
         return self.render_to_response({'module': module})
+=======
+        return self.render_to_response({'module': module})
+    
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filters(id=id, course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved':'OK'})
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+from django.db.models import Count
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course 
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses_created'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+
+        return self.render_to_response({'subjects': subjects, 'subject': subject, 'courses': courses})
+
+from django.views.generic.detail import DetailView
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
+>>>>>>> d43ad7c (Implement CourseListView and CourseDetailView for displaying available courses to public)
