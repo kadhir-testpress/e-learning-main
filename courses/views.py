7c40a4c22ce810d8df12116ee74a90fcbf089b4c
kadhir <kadhir@kadhir.com>
2024-03-08 18:09:24 +0530

    Add Course Enrollment Functionality for Students

--- courses/views.py
+++ courses/views.py
@@ -10,6 +10,7 @@ from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+from students.forms import CourseEnrollForm
 
 # Create your views here.
 class OwnerMixin(object):
@@ -153,3 +154,8 @@ from django.views.generic.detail import DetailView
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(initial = {'course': self.object})
+        return context
