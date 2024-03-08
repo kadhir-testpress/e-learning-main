9a0ba168f5ed59f8c02c6b0fc7495632cf2af462
kadhir <kadhir@kadhir.com>
2024-03-08 15:31:05 +0530

    Revert "jQuery UI Integration for Reorder Modules and Contents"
    This reverts commit 43dd31c1c43c74e266132f7f2d7654b3ce525610.

--- courses/urls.py
+++ courses/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('<pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
     path('<pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
     path('<pk>/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update'),
-
+    
     path('module/<int:module_id>/content/<model_name>/create/', 
          views.ContentCreateUpdateView.as_view(), 
          name='module_content_create'),
@@ -23,14 +23,4 @@ urlpatterns = [
     path('module/<int:module_id>/',
          views.ModuleContentListView.as_view(),
          name='module_content_list'),
-    
-    path('module/order/',
-         views.ModuleOrderView.as_view(),
-         name='module_order'),
-    
-    path('content/order/',
-         views.ContentOrderView.as_view(),
-         name='content_order'),
-
-    
 ]
